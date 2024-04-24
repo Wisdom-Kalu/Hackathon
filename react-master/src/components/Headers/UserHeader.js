2c1c08774341fc07fccebebedc6e4153931fbe2e
nawshin81 <nawshinlfat@iut-dhaka.edu>
2024-04-24 19:10:42 +0300

    questions page modified

--- react-master/src/components/Headers/UserHeader.js
+++ react-master/src/components/Headers/UserHeader.js
@@ -8,20 +8,13 @@ const UserHeader = () => {
     <>
       <div
         className="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-        style={{
-          minHeight: "600px",
-          backgroundImage:
-            "url(" + require("../../assets/img/theme/profile-cover.jpg") + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "center top",
-        }}
       >
         {/* Mask */}
         <span className="mask bg-gradient-default opacity-8" />
         {/* Header container */}
         <Container className="d-flex align-items-center" fluid>
           <Row>
-            <Col lg="7" md="10">
+            {/* <Col lg="7" md="10">
               <h1 className="display-2 text-white">Hello Jesse</h1>
               <p className="text-white mt-0 mb-5">
                 This is your profile page. You can see the progress you've made
@@ -34,7 +27,7 @@ const UserHeader = () => {
               >
                 Edit profile
               </Button>
-            </Col>
+            </Col> */}
           </Row>
         </Container>
       </div>
