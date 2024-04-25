f6d6c9b4a02c8cfb17a845735b51f607b4f5b36d
nawshin81 <nawshinlfat@iut-dhaka.edu>
2024-04-25 19:00:55 +0300

    conflict fixed

--- src/views/examples/Manual.js
+++ src/views/examples/Manual.js
@@ -16,100 +16,10 @@ import {
   Table,
 } from "reactstrap";
 import { CSVLink } from "react-csv";
-import { Container, Row, Card, CardBody, CardTitle, CardText, CardImg, Table } from 'reactstrap';
+
 //import { CSVLink } from 'react-csv';
 
-// const MapWrapper = () => {
-//   const mapRef = React.useRef(null);
-//   React.useEffect(() => {
-//     let google = window.google;
-//     let map = mapRef.current;
-//     let lat = "40.748817";
-//     let lng = "-73.985428";
-//     const myLatlng = new google.maps.LatLng(lat, lng);
-//     const mapOptions = {
-//       zoom: 12,
-//       center: myLatlng,
-//       scrollwheel: false,
-//       zoomControl: true,
-//       styles: [
-//         {
-//           featureType: "administrative",
-//           elementType: "labels.text.fill",
-//           stylers: [{ color: "#444444" }],
-//         },
-//         {
-//           featureType: "landscape",
-//           elementType: "all",
-//           stylers: [{ color: "#f2f2f2" }],
-//         },
-//         {
-//           featureType: "poi",
-//           elementType: "all",
-//           stylers: [{ visibility: "off" }],
-//         },
-//         {
-//           featureType: "road",
-//           elementType: "all",
-//           stylers: [{ saturation: -100 }, { lightness: 45 }],
-//         },
-//         {
-//           featureType: "road.highway",
-//           elementType: "all",
-//           stylers: [{ visibility: "simplified" }],
-//         },
-//         {
-//           featureType: "road.arterial",
-//           elementType: "labels.icon",
-//           stylers: [{ visibility: "off" }],
-//         },
-//         {
-//           featureType: "transit",
-//           elementType: "all",
-//           stylers: [{ visibility: "off" }],
-//         },
-//         {
-//           featureType: "water",
-//           elementType: "all",
-//           stylers: [{ color: "#5e72e4" }, { visibility: "on" }],
-//         },
-//       ],
-//     };
-
-//     map = new google.maps.Map(map, mapOptions);
-
-//     const marker = new google.maps.Marker({
-//       position: myLatlng,
-//       map: map,
-//       animation: google.maps.Animation.DROP,
-//       title: "Light Bootstrap Dashboard PRO React!",
-//     });
-
-//     const contentString =
-//       '<div class="info-window-content"><h2>Light Bootstrap Dashboard PRO React</h2>' +
-//       "<p>A premium Admin for React-Bootstrap, Bootstrap, React, and React Hooks.</p></div>";
-
-//     const infowindow = new google.maps.InfoWindow({
-//       content: contentString,
-//     });
-
-//     google.maps.event.addListener(marker, "click", function () {
-//       infowindow.open(map, marker);
-//     });
-//   }, []);
-//   return (
-//     <>
-//       <div
-//         style={{ height: `600px` }}
-//         className="map-canvas"
-//         id="map-canvas"
-//         ref={mapRef}
-//       ></div>
-//     </>
-//   );
-// };
-
-const Maps = () => {
+const Manual = () => {
   const text1 = (
     <div>
       <p>
@@ -275,4 +185,4 @@ const Maps = () => {
   );
 };
 
-export default Maps;
+export default Manual;
