38ac4ccd0c6c034fb5f6158747a9953c13781f4a
wisdom.kalu <wisdom.kalu@ashesi.edu.gh>
2024-04-25 15:34:51 +0300

    moved files to main page in Github

---
+++ src/routes.js
@@ -0,0 +1,48 @@
+import Register from "views/examples/Register";
+import Manual from "views/examples/Manual.js";
+import Questions from "views/examples/Questions.js";
+import Roles from "views/examples/Roles.js";
+import Home from "views/examples/Home";
+import Questionnaire from "views/examples/Questionnaire.js";
+
+// test for push
+
+var routes = [
+  {
+    path: "/home",
+    name: "Home",
+    icon: "ni ni-shop text-blue",
+    component: <Home />,
+    layout: "/admin",
+  },
+  {
+    path: "/manual",
+    name: "Manual",
+    icon: "ni ni-book-bookmark text-orange",
+    component: <Manual />,
+    layout: "/admin",
+  },
+  {
+    path: "/question",
+    name: "Questions",
+    icon: "ni ni-chat-round text-green",
+    component: <Questions />,
+    layout: "/admin",
+  },
+  {
+    path: "/questionnaire",
+    name: "Questionnaire",
+    icon: "ni ni-chat-round text-green",
+    component: <Questionnaire />,
+    layout: "/admin",
+  },
+  // {
+  //   path: "/roles",
+  //   name: "Roles",
+  //   icon: "ni ni-settings-gear-65 text-purple",
+  //   component: <Roles />,
+  //   layout: "/admin",
+  // },
+];
+
+export default routes;
