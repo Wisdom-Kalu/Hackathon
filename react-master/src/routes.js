bebfca29293f5a091dd5b68642031040ab55efaf
itgirl97 <nimrah.yousuf97@gmail.com>
2024-04-25 11:12:15 +0300

    resolving conflict

--- react-master/src/routes.js
+++ react-master/src/routes.js
@@ -2,6 +2,7 @@ import Register from "views/examples/Register";
 import Manual from "views/examples/Manual.js";
 import Questions from "views/examples/Questions.js";
 import Roles from "views/examples/Roles.js";
+import Questionnaire from "views/examples/Questionnaire.js";
 
 // test for push
 
@@ -21,10 +22,17 @@ var routes = [
     layout: "/admin",
   },
   {
-    path: "/questions",
+    path: "/question",
     name: "Questions",
     icon: "ni ni-chat-round text-green",
     component: <Questions />,
+    layout: "/admin",
+  },
+  {
+    path: "/questionnaire",
+    name: "Questionnaire",
+    icon: "ni ni-chat-round text-green",
+    component: <Questionnaire />,
     layout: "/admin",
   },
   {
