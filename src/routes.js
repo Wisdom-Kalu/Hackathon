d80722b5aef82f41bb20e33e38334ba980a26ea0
wisdom.kalu <wisdom.kalu@ashesi.edu.gh>
2024-04-25 17:43:10 +0300

    test update

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import Register from "views/examples/Register";
+import Manual from "views/examples/Manual.js";
+import Questions from "views/examples/Questions.js";
+import Roles from "views/examples/Roles.js";
+
+// test for push
+
+var routes = [
+  {
+    path: "/register",
+    name: "Home",
+    icon: "ni ni-shop text-blue",
+    component: <Register />,
+    layout: "/auth",
+  },
+  {
+    path: "/manual",
+    name: "Manual",
+    icon: "ni ni-book-bookmark text-orange",
+    component: <Manual />,
+    layout: "/admin",
+  },
+  {
+    path: "/questions",
+    name: "Questions",
+    icon: "ni ni-chat-round text-green",
+    component: <Questions />,
+    layout: "/admin",
+  },
+  {
+    path: "/roles",
+    name: "Roles",
+    icon: "ni ni-settings-gear-65 text-purple",
+    component: <Roles />,
+    layout: "/admin",
+  },
+];
+
+export default routes;
